ed2dd9b2538c0d42c8f2ffa3e9bc1d39f694e40d
Leonardo V Pereira <leonardo.vpereira@al.infnet.edu.br>
2025-11-14 06:46:00 -0300

    add safeareaview

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import { Stack } from "expo-router";
 import { NativeBaseProvider } from "native-base";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Navbar from "../components/Navbar";
 import { TratamentosProvider } from "../context/TratamentosContext";
 
 export default function RootLayout() {
   return (
-    <NativeBaseProvider>
-      <TratamentosProvider>
-        <Navbar />
-        <Stack screenOptions={{ headerShown: false }} />
-      </TratamentosProvider>
-    </NativeBaseProvider>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NativeBaseProvider>
+        <TratamentosProvider>
+          <Navbar />
+          <Stack screenOptions={{ headerShown: false }} />
+        </TratamentosProvider>
+      </NativeBaseProvider>
+    </SafeAreaView>
   );
 }
